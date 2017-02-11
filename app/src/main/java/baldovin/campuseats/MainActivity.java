82d39863b5629c169bad5986b0f4b401246dc60a
cjb5326 <cjb5326@cs.rit.edu>
2017-02-11 17:41:59 -0500

    -Added splash screen -Created CampusEats logo

--- app/src/main/java/baldovin/campuseats/MainActivity.java
+++ app/src/main/java/baldovin/campuseats/MainActivity.java
@@ -1,13 +1,23 @@
 package baldovin.campuseats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
+
     }
+
 }
