307334668809834e7b521e4401bda6b35d7350c1
cjb5326 <cjb5326@cs.rit.edu>
2017-02-11 19:21:24 -0500

    -Began listview for dining locations

---
+++ app/src/main/java/baldovin/campuseats/SchoolSelection.java
@@ -0,0 +1,13 @@
+package baldovin.campuseats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SchoolSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school_selection);
+    }
+}
